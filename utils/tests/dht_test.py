80d7afc8113b02b5670c57e544eff9c9c96dc36f
coll6 <ortegataj@hotmail.com>
2024-09-01 22:00:09 -0600

    Working version of dht_sensor

--- utils/tests/dht_test.py
+++ utils/tests/dht_test.py
@@ -14,16 +14,6 @@ def dht_test():
 	#Instantiate DHTSensor object using pi GPIO 17 here
 	sensor = DHTSensor(pin=17)
 	
-#	GPIO.setup(17, GPIO.OUT)
-
-#	GPIO.output(17, GPIO.HIGH)
-#	try:
-#		GPIO.output(17, GPIO.LOW)
-#		print(f"pin 17 is  low")
-#		input("Press enter to exit")
-
-#	finally:
-#		GPIO.cleanup()
 	#Read the sensor data
 	success, temperature, humidity, error_message = sensor.read_sensor_data()
 
@@ -33,7 +23,6 @@ def dht_test():
 		print("Temperature: {:.1f}°C".format(temperature))
 		print("Humidity: {:.1f}%".format(humidity))
 	else:
-		print("Failed to read sensor data")
+		print("{error_message}")
 if __name__ == "__main__":
 	dht_test()
-#	GPIO.cleanup()
