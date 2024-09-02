56eb9876f30ca7a67573a6759a6bc49e802a46d2
coll6 <ortegataj@hotmail.com>
2024-09-01 18:30:47 -0600

    Changed to polling method. Pulse Length test made.

--- utils/tests/dht11_timing_test.py
+++ utils/tests/dht11_timing_test.py
@@ -9,39 +9,36 @@ def inited():
 	print("pin startup")
 	GPIO.setmode(GPIO.BCM)
 def start_com():
-	print("Starting communication.")
 	GPIO.setup(pin, GPIO.OUT)
 	GPIO.output(pin, GPIO.HIGH)
-#	GPIO.add_event_detect(pin, GPIO.BOTH, callback=detected_edge)
-
+	print(f"Starting communication.{time.perf_counter_ns()}")
 	GPIO.output(pin, GPIO.LOW)
-	time.sleep(0.018) #sleep 18ms
-	GPIO.setup(pin,GPIO.IN)
-	GPIO.add_event_detect(pin,GPIO.BOTH,callback=detected_edge)
-def detected_edge(channel):
+	time.sleep(0.018) #Sleep for 18ms
+def poll_for_pulses():
 	global pulse_start_time_ns, pulse_lengths
-	cur_time = time.time() * 1_000_000
-	if GPIO.input(17) == GPIO.HIGH:
-		if pulse_start_time_ns is None:
-			print(f"low to high: {time.time()}")
-			pulse_start_time_ns = cur_time
-	else:
-		#print("High to low")
-		if pulse_start_time_ns is not None:
-			print("High to low")
-			pulse_length = cur_time - pulse_start_time_ns
-			pulse_lengths.append(pulse_length)
-			pulse_start_time_ns = None
-#def read():
-#	GPIO.setup(pin, GPIO.IN)
-#	GPIO.add_event_detect(pin, GPIO.BOTH, callback=detected_edge)
+	print(f"Starting Read.{time.perf_counter_ns()}")
+	GPIO.setup(pin, GPIO.IN)
+
+	while True:
+		current_state = GPIO.input(pin)
+		cur_time = time.perf_counter_ns()
+
+		if current_state == GPIO.HIGH:
+			if pulse_start_time_ns is None:
+				pulse_start_time_ns = cur_time
+		else:
+			if pulse_start_time_ns is not None:
+				pulse_length = cur_time - pulse_start_time_ns
+				pulse_lengths.append(pulse_length)
+				pulse_start_time_ns = None
+		if pulse_start_time_ns is not None and (cur_time - pulse_start_time_ns) >= 100000:
+			print("Read done.")
+			break
 if __name__ == "__main__":
 	inited()
 	start_com()
-#	read()
-	time.sleep(2)
-	GPIO.remove_event_detect(pin)
-	print("Finished read")
-	for length in pulse_lengths:
-		print(f"Pulse Length: {length:.2f} us")
+	poll_for_pulses()
+	print("Pulse lengths (in microseconds?):")
+	for i, length in enumerate(pulse_lengths):
+		print(f"Pulse {i + 1}: {length / 1000} microseconds.")
 	GPIO.cleanup()
