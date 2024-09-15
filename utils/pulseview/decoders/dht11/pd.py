16ac99ecca96c939cb9393f48af1b10792953738
coll6 <ortegataj@hotmail.com>
2024-09-15 14:19:50 -0600

    Added more flags complete decoding.

--- utils/pulseview/decoders/dht11/pd.py
+++ utils/pulseview/decoders/dht11/pd.py
@@ -32,14 +32,23 @@ class Decoder(srd.Decoder):
     outputs = []
     tags = ['IC', 'Sensor']
     annotations = (
-        ('start', 'Start of Communication'),
+        ('start', 'Start'),
+        ('response', 'Response'),
+        ('bit', 'Bit'),
+        ('end', 'End'),
         ('byte', 'Byte'),
-        ('byte2', 'Byte2'),
+        ('humidity', 'Relative humidity'),
+        ('temperature', 'Temperature'),
+        ('checksum', 'Checksum'),
+        ('wait', 'Wait'),
+        ('calculated checksum', 'Calculated Checksum'),
+        ('validity', 'Validity'),
     )
     annotation_rows = (
-        ('control', 'Control Signals', (0,)),  # Shows the 'start' annotation in a row called 'Control Signals'
-        ('data', 'Data', (1,)), 
-        ('data2', 'Data2', (2,)),
+        ('bits', 'Bits', (0, 1, 2, 3, 8)),
+        ('bytes', 'Bytes', (4, 7)),
+        ('results', 'Results', (5, 6)),
+        ('validation','Validation', (9, 10)),
     )
     channels = (
         {'id': 'sda', 'name': 'SDA', 'desc': 'Single wire serial data line'},
@@ -59,14 +68,26 @@ class Decoder(srd.Decoder):
         self.out_ann = self.register(srd.OUTPUT_ANN)
         
     def procdata(self, bitlist, bytepos):
-        if len(bitlist) == 40:
+        if len(bitlist) == 40: #Decode for dht11 will have 40 valid bits
+            humid = int(''.join(str(bit) for bit in bitlist[0:8]),2)  + (int(''.join(str(bit) for bit in bitlist[9:16]),2) / 10.0)
+            temp = int(''.join(str(bit) for bit in bitlist[17:24]),2)  + (int(''.join(str(bit) for bit in bitlist[25:32]),2) / 10.0)
+            cal_sum = int(''.join(str(bit) for bit in bitlist[0:8]),2) + int(''.join(str(bit) for bit in bitlist[9:16]),2) + int(''.join(str(bit) for bit in bitlist[17:24]),2) + int(''.join(str(bit) for bit in bitlist[25:32]),2)
             for i in range(0, len(bytepos), 2):
                 strt_bit_index = (i // 2) * 8
                 end_bit_index = strt_bit_index + 8
                 sublist = bitlist[strt_bit_index:end_bit_index] 
-                bit_string = ''.join(str(bit) for bit in sublist)
-                self.put(bytepos[i], bytepos[i + 1], self.out_ann, [1, [str(int(bit_string, 2))]])
-        
+                bit_string = int(''.join(str(bit) for bit in sublist),2)
+                if i in [0, 2, 4, 6]:
+                    self.put(bytepos[i], bytepos[i + 1], self.out_ann, [4, ['Byte: %#04x' % bit_string, '%#04x' % bit_string]])
+                else:
+                    self.put(bytepos[i], bytepos[i + 1], self.out_ann, [7, ['Checksum: %#04x' % bit_string, '%#04x' % bit_string]])
+                    if bit_string == cal_sum:
+                        self.put(bytepos[i], bytepos[i + 1], self.out_ann, [10, ['Checksum: %#04x: OK' % bit_string, '%#04x: OK' % bit_string]])
+                    else:
+                        self.put(bytepos[i], bytepos[i + 1], self.out_ann, [10, ['Checksum: %#04x: Error' % bit_string, '%#04x: Err' % bit_string]])
+            self.put(bytepos[0], bytepos[3], self.out_ann, [5, ['Humidity: %.1f %%' % humid, 'RH = %.1f %%' % humid]])
+            self.put(bytepos[4], bytepos[7], self.out_ann, [6, ['Temperature: %.1f °C' % temp, 'T = %.1f °C' % temp]])
+            self.put(bytepos[0], bytepos[7], self.out_ann, [9, ['Calculated sum: %#04x' % cal_sum, '%#04x' % cal_sum]])
     def decode(self):
         state = 'IDLE'
         samp_pin = None
@@ -79,7 +100,7 @@ class Decoder(srd.Decoder):
         pins = self.wait()
         samp_pin = pins[0]
         while True:
-            if state == 'IDLE': #Wait for a logic low
+            if state == 'IDLE': #Wait for a logic low starting is communication
                 #raise SamplerateError(str(self.samplenum) + ' ' + str(pins))
                 if samp_pin == False:   #Checks if the logic level is low
                     start_samp = self.samplenum
@@ -92,7 +113,7 @@ class Decoder(srd.Decoder):
                 prev_samp = self.samplenum
                 pins = self.wait({0: 'r'}) #Since we must be logic low look for the next rising edge.
                 if (self.samplenum - prev_samp)  >= ((18000/1000000) * self.samplerate):
-                    self.put(prev_samp, self.samplenum, self.out_ann, [0, ['start ' + str(self.samplenum) + ' ' + str(self.samplerate)]])
+                    self.put(prev_samp, self.samplenum, self.out_ann, [0, ['start']])
                     prev_samp = self.samplenum
                     state = 'WAIT_RESP'
                 else:
@@ -100,7 +121,7 @@ class Decoder(srd.Decoder):
             elif state == 'WAIT_RESP': #Small wait before there is a response from the sensor
                 pins = self.wait({0: 'e'}) #Look for the next edge
                 if pins[0] == False and (((8/1000000) * self.samplerate)) <= (self.samplenum - prev_samp) <= (((40/1000000) * self.samplerate)):
-                    self.put(prev_samp, self.samplenum, self.out_ann, [0, ['wait']])
+                    self.put(prev_samp, self.samplenum, self.out_ann, [8, ['Wait', 'W']])
                     prev_samp = self.samplenum
                     state = 'RESP'           
                 else:
@@ -108,7 +129,7 @@ class Decoder(srd.Decoder):
             elif state == 'RESP': #Checking for the response signal.
                 pins = self.wait({0: 'e'})
                 if  pins[0] == True and (((80 * (1 - 0.10)/1000000) * self.samplerate)) <= (self.samplenum - prev_samp) <= (((80 * (1 + 0.10)/1000000) * self.samplerate)):
-                    self.put(prev_samp, self.samplenum, self.out_ann, [0, ['Reponse']])
+                    self.put(prev_samp, self.samplenum, self.out_ann, [1, ['Response', 'R']])
                     prev_samp = self.samplenum
                     state = 'WAIT_DATA'
                 else:
@@ -116,12 +137,12 @@ class Decoder(srd.Decoder):
             elif state == 'WAIT_DATA':
                 pins = self.wait({0: 'e'}) #Wait before data response
                 if pins[0] == False and (self.samplenum - prev_samp) >= ((75/1000000) * self.samplerate):    
-                    self.put(prev_samp, self.samplenum, self.out_ann, [0, ['Wait']])
+                    self.put(prev_samp, self.samplenum, self.out_ann, [8, ['Wait', 'W']])
                     prev_samp = self.samplenum
                     state = 'COMS'
                 else:
                     state = 'IDLE'
-            elif state == 'COMS':
+            elif state == 'COMS': #Data response
                 data_bits = []
                 byte_loc = []
                 inter_samp = None
@@ -129,27 +150,27 @@ class Decoder(srd.Decoder):
                 #int(-(-x // 1)) used to round a number without importing the math module.
                 end_lng = int(-(-((((70 + 1) * (1 + 0.13)/1000000) * self.samplerate)) + 1 // 1))
                 while True:
-                    pins = self.wait([{0: 'e'}, {'skip': end_lng}])
+                    pins = self.wait([{0: 'e'}, {'skip': end_lng}]) #Look for the next edge or longer than a '1' bit value
                     if pins[0] == True:
                         inter_samp = self.samplenum
                         if (inter_samp - prev_samp) > end_lng:
-                            self.put(prev_samp, self.samplenum, self.out_ann, [0, ['End Com ' + str(len(byte_loc))]])
+                            self.put(prev_samp, self.samplenum, self.out_ann, [3, ['End', 'E']])
                             state = 'IDLE'
-                            self.procdata(data_bits, byte_loc)
+                            self.procdata(data_bits, byte_loc) #Process the collected bits
                             break
                     elif pins[0] == False:
                         diff = self.samplenum - inter_samp
                         if (((25 * (1 - 0.20)/1000000) * self.samplerate)) <= diff <= (((25 * (1 + 0.20)/1000000) * self.samplerate)):
-                            self.put(prev_samp, self.samplenum, self.out_ann, [0, ['0']])
+                            self.put(prev_samp, self.samplenum, self.out_ann, [2, ['Bit: 0','0']])
                             data_bits.append(0)
                         elif (((70 * (1 - 0.10)/1000000) * self.samplerate)) <= diff <= (((70 * (1 + 0.13)/1000000) * self.samplerate)):
-                            self.put(prev_samp, self.samplenum, self.out_ann, [0, ['1']])
+                            self.put(prev_samp, self.samplenum, self.out_ann, [2, ['Bit: 1','1']])
                             data_bits.append(1)
                         else:
                             state = 'IDLE'
                             break
-                        if len(data_bits) % 8 == 1:
+                        if len(data_bits) % 8 == 1: #For values 1, 9... to determine ending position of byte
                             byte_loc.append(prev_samp)
-                        if len(data_bits) % 8 == 0:
+                        if len(data_bits) % 8 == 0: #For values 0, 8... to determine starting position of byte
                             byte_loc.append(self.samplenum)
                         prev_samp = self.samplenum
